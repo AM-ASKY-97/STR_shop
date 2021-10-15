55fab0fd03af30a5eb5d4f111f383b1edb583b07
97PHPDeveloper <am.asky97@gmail.com>
2021-10-15 16:44:21 +0530

    cange

--- payment.php
+++ payment.php
@@ -82,21 +82,50 @@
 
         <div class="row ">
           <div class="col-lg-6 d-block mx-auto">
-          <div class="jumbotron bg-light">
-            <h2>Secure Payment</h2>
+          <div class="jumbotron mt-5 bg-light">
             <form>
-              <div class="row">
-                <div class="col-md-6 ">
-                  <label for="">Card Holders Name </label>
-                  <input type="text">
+                <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">Card Number</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Card Number">
                 </div>
 
-                <div class="col-md-6">
-                  <label for="">Card Number</label>
-                  <input type="text">
+                  <div class="form-group col-md-6">
+                    <label for="inputEmail4">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                  </div>
+                  
+                  <div class="form-group col-md-6">
+                    <label for="inputEmail4">Expiry</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Expiry">
+                  </div>
+
+                  <div class="form-group col-md-6">
+                    <label for="inputEmail4">CVC</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="CVC">
+                  </div>
+                  
                 </div>
-              </div>   
-            </form>
+                <div class="form-group">
+                    <label for="inputAddress">Mobile Number</label>
+                    <input type="text" class="form-control" id="inputAddress" placeholder="000-0000000">
+                  </div>
+
+                <div class="form-group">
+                  <label for="inputAddress">Address</label>
+                  <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                </div>
+                
+                <div class="form-group">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                      Check me out
+                    </label>
+                  </div>
+                </div>
+                <button type="submit" class="btn btn-primary w-100">CONTINUE</button>
+              </form>
         </div>
           </div>
         </div>
